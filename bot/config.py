5f906e176e02f98556535111bc20a5f99a594cbc
GreyMatter's <greymattersyt@gmail.com>
2022-11-05 14:08:25 +0530

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,31 @@
+import os
+
+class Config:
+
+    BOT_TOKEN = ""
+
+    SESSION_NAME = ""
+
+    API_ID = ""
+
+    API_HASH = ""
+
+    CLIENT_ID = ""
+
+    CLIENT_SECRET = ""
+
+    AUTH_USERS = [942731625]
+
+    VIDEO_DESCRIPTION = ""
+
+    VIDEO_CATEGORY = ""
+
+    VIDEO_TITLE_PREFIX = ""
+
+    VIDEO_TITLE_SUFFIX = ""
+    
+    DEBUG = bool()
+
+    UPLOAD_MODE = "unlisted"
+    
+    CRED_FILE = "auth_token.txt"
