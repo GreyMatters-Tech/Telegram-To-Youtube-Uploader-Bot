6a9ce1aa72e2ca932dec3c14b7ca959e5c8362d6
Choudharyboy <115158030+Choudharyboy@users.noreply.github.com>
2022-11-09 10:39:22 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,27 +2,27 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "5683916988:AAEmdqUZCQYRbkwUsASSAnpNdamBahWy6z0"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "anuchoudharyyybot"
 
-    API_ID = ""
+    API_ID = "18451131"
 
-    API_HASH = ""
+    API_HASH = "1aff8e25d4fd3553ee85ced886f90c655"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "http://112813369670-uh4rgj4qgilgk4er78lbojuo6e3qcone.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [5651503575]
 
     VIDEO_DESCRIPTION = ""
 
     VIDEO_CATEGORY = ""
 
-    VIDEO_TITLE_PREFIX = ""
+    VIDEO_TITLE_PREFIX = "walcome"
 
-    VIDEO_TITLE_SUFFIX = ""
+    VIDEO_TITLE_SUFFIX = "thank"
     
     DEBUG = bool()
 
